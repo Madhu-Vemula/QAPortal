a24c65cb5d81d2df53af82e317e8a0368ea79e7d
Madhu Vemula <madhu.vemula@pal.tech>
2025-10-23 17:12:02 +0530

    Added removing token

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import {  useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "../src/styles/login-form.css"
 import "../src/styles/buttons.css"
 import "../src/styles/form-container.css"
@@ -14,19 +14,34 @@ import "../src/styles/google-login.css"
 import { useEffect } from "react";
 import { useGetCurrentUserQuery } from "./services/authService";
 import type { RootState } from "./store/store";
+import { logout } from "./store/authSlice";
 
 function App() {
   const token = useSelector((state: RootState) => state.auth.token);
+  const dispatch = useDispatch()
   const { refetch } = useGetCurrentUserQuery(undefined, {
     skip: !token,
   });
   useEffect(() => {
     document.title = "Q&A Portal";
-    if (token) refetch();
+    const refetchUser = async () => {
+      if (token) {
+        try {
+          await refetch().unwrap();
+        }
+        catch (error: any) {
+          if (error.data.message === "Session expired. Please login again.") {
+            dispatch(logout())
+          }
+          console.error('Google Login Failed:', error.data.message);
+        }
+      }
+    }
+    refetchUser()
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", async () => {
         try {
-           await navigator.serviceWorker.register("/sw.js");
+          await navigator.serviceWorker.register("/sw.js");
         } catch (err) {
           console.error("Service Worker registration failed:", err);
         }
